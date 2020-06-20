5e5411cb5b17bd23a548c90f03f12093bc7dc2ce
vipul <vipuljain906@gmail.com>
2020-06-20 17:36:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { Route, Switch } from "react-router-dom";
+
+import Dashboard from "./components/Dashboard";
+// Main Routes file
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+
+import appReducer from "./containers/reducers";
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const rootReducer = (state, action) => {
+  return appReducer(state, action);
+};
+
+const store =
+  process.env.NODE_ENV === "development"
+    ? createStore(
+        rootReducer,
+        initialState,
+        compose(applyMiddleware(...middleware))
+      )
+    : createStore(
+        rootReducer,
+        initialState,
+        compose(applyMiddleware(...middleware))
+      );
+
+function App() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Route exact path="/" name="Dashboard" component={Dashboard} />
+      </BrowserRouter>
+    </Provider>
+  );
+}
+
+export default App;
